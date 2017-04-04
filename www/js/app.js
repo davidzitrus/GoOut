7a68f68057a2ac43582bc1cc153d5a2374ecab5f
davidzitrus <davmit@web.de>
2017-04-04 17:49:12 +0200

    V04.04 Navigation, Top Bar removed

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-// Ionic Starter App
+﻿// Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
@@ -90,6 +90,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/detail');
 
 });
